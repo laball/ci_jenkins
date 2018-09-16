8b8ab6569a0d78fc0f3b22907e04492fcacbf639
Lee <libo1573@163.com>
2018-09-16 23:00:32 +0800

    初始化

---
+++ WebApp.Lib/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace WebApp.Lib
+{
+    public class Test
+    {
+        public static void ShowLog(string message)
+        {
+            Trace.WriteLine(message);
+        }
+
+    }
+}
